9a320584bb5cb76b0c5081e9c30a9458b03b0d07
Maria Clara <mcbarroso.contato@gmail.com>
2021-05-03 09:44:58 -0300

    First test about faker and factory

--- ieat/spec/model/product_spec.rb
+++ ieat/spec/model/product_spec.rb
@@ -1,6 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Product do
-  it { should belongs_to(:restaurant) }
-  it { should validade_presence_of :name }
 end
--- ieat/spec/model/restaurant_spec.rb
+++ ieat/spec/model/restaurant_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant do
-  it { should have_many(:products) }
   context 'testing total of products' do
+    let(:restaurant) { FactoryBot.create(:restaurant) }
+
+    before do
+      product1 = FactoryBot.create(:product, restaurant: restaurant)
+      product2 = FactoryBot.create(:product, restaurant: restaurant)
+      product3 = FactoryBot.create(:product, restaurant: restaurant)
+    end
     it 'returns total of products for a specific restaurant' do
-      restaurant = Restaurant.create(name: 'restaurant test', address: 'anything...')
-
-      product = Product.create(restaurant: restaurant, name: 'prato', value: 15)
-      product1 = Product.create(restaurant: restaurant, name: 'product', value: 10)
-      product1 = Product.create(restaurant: restaurant, name: 'anything', value: 20)
-
       expect(restaurant.total_of_products).to eq 3
     end
   end
